6d910e30e3806172c1b642b7cd8f754fa8d0dc89
Asark0 <128174148+Asark0@users.noreply.github.com>
2023-03-22 14:04:18 -0400

    Add files via upload
    DATA CLEANING OVERVIEW
    Review the data: Understand what information it contains, what format it is in, and what types of issues might be present.
    
    Identify missing data: Check for missing values in the data and determine the how to fix the values, whether it's imputing values or deleting rows
    
    Standardize the data: Check for inconsistencies in formatting, spelling, and capitalization and standardize the data to make it consistent across the dataset.
    
    Remove duplicates: Identify and remove any duplicate records in the data, as these can skew analysis results.
    
    Check for outliers: Examine the data for outliers, or data points that fall outside the expected range, and determine whether they should be removed or kept in the dataset.
    
    Validate the data: Check that the data makes sense and meets the expected criteria, using domain knowledge and data profiling techniques.
    
    Document the cleaning process: Keep a record of the cleaning process, including any decisions made and the reasoning behind them, to ensure transparency and reproducibility.

---
+++ Cleaned Dim_Internet Sales.sql
@@ -0,0 +1,35 @@
+--Cleaned Dim_Internet Sales--
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey] -- ,[PromotionKey]
+  --,[CurrencyKey]
+  -- ,[SalesTerritoryKey]
+  , 
+  [SalesOrderNumber] -- ,[SalesOrderLineNumber]
+  --  ,[RevisionNumber]
+  -- ,[OrderQuantity]
+  --,[UnitPrice]
+  --,[ExtendedAmount]
+  --,[UnitPriceDiscountPct]
+  --,[DiscountAmount]
+  -- ,[ProductStandardCost]
+  -- ,[TotalProductCost]
+  , 
+  [SalesAmount] --,[TaxAmt]
+  --,[Freight]
+  -- ,[CarrierTrackingNumber]
+  -- ,[CustomerPONumber]
+  -- ,[OrderDate]
+  --,[DueDate]
+  -- ,[ShipDate]
+FROM 
+  [dbo].[FactInternetSales] 
+WHERE 
+  LEFT(OrderDateKey, 4) >= Year(
+    GETDATE()
+  ) -2 --Makes sure that only two years of date come from extraction
+ORDER by 
+  OrderDateKey asc
